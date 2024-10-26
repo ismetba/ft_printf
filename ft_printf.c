cf78cf4ba73f14d0150d99c76474212b98fe6025
Ismet Bayandur <ibayandu@k1m10s06.42istanbul.com.tr>
2024-10-26 22:49:42 +0300

    end of day 26.10.2024

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: ibayandu <ibayandu@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 20:48:02 by ibayandu          #+#    #+#             */
-/*   Updated: 2024/10/25 22:31:51 by ibayandu         ###   ########.fr       */
+/*   Updated: 2024/10/26 22:48:21 by ibayandu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,67 @@
 #include <limits.h>
 #include <stdarg.h>
 
-int static	count_of_persont_sign(char *ptr)
+static char	*show_hex(char *str, int is_upper)
+{
+	if (is_upper)
+		return (ft_strjoin("0X", str));
+	return (ft_strjoin("0x", str));
+}
+
+static int	nbr_root_sixteen(long number)
+{
+	int	i;
+
+	i = 1;
+	while (number > 16)
+	{
+		i++;
+		number = number / 16;
+	}
+	return (i);
+}
+
+char static	*convert_to_hex(long number, int is_upper)
+{
+	char	*result;
+	char	*hexchars;
+	int		loop;
+
+	loop = nbr_root_sixteen(number);
+	result = malloc(loop);
+	hexchars = "0123456789abcdef";
+	while (loop--)
+	{
+		result[loop] = hexchars[number % 16];
+		if (is_upper)
+			result[loop] = ft_toupper(result[loop]);
+		number /= 16;
+	}
+	return (result);
+}
+
+int static	func_handles(va_list args, char *str)
+{
+	if (*str == 's')
+		ft_putstr_fd(va_arg(args, char *), 1);
+	else if (*str == 'i')
+		ft_putnbr_fd(va_arg(args, int), 1);
+	else if (*str == 'c')
+		ft_putchar_fd(va_arg(args, int), 1);
+	else if (*str == 'p')
+		ft_putstr_fd(show_hex(convert_to_hex(va_arg(args, long), 0), 0), 1);
+	else if (*str == 'd')
+		ft_putnbr_fd(va_arg(args, int), 1);
+	else if (*str == 'u')
+		ft_putnbr_fd(va_arg(args, int), 1);
+	else if (*str == 'x')
+		ft_putstr_fd(convert_to_hex(va_arg(args, long), 0), 1);
+	else if (*str == 'X')
+		ft_putstr_fd(convert_to_hex(va_arg(args, long), 1), 1);
+	return (0);
+}
+
+int static	count_persent(char *ptr)
 {
 	size_t	count;
 
@@ -28,15 +88,16 @@ int static	count_of_persont_sign(char *ptr)
 
 void	print_func(char *str, va_list args)
 {
-	if (!(count_of_persont_sign(str - 1) % 2) && *str != '%')
+	if (!(count_persent(str - 1) % 2) && *str != '%')
 	{
 		ft_putchar_fd(*str, 1);
 	}
-	else if (*str == '%' && *(str + 1) == '%' && (count_of_persont_sign(str)
-			% 2))
+	else if (*str == '%' && *(str + 1) == '%' && (count_persent(str) % 2))
+	{
 		ft_putchar_fd(*str, 1);
+	}
 	else if (*str != '%')
-		ft_putstr_fd(va_arg(args, char *), 1);
+		func_handles(args, str);
 }
 
 int	ft_printf(const char *str, ...)
@@ -55,5 +116,10 @@ int	ft_printf(const char *str, ...)
 
 int	main(void)
 {
-	ft_printf("aadf %s %sdfsdfsdf%%%%s\n", "deneme", "falan", "filan");
+	char	str[] = "deneme";
+
+	ft_printf("%c %s %p %d %i %u %x %X %%\n", 'c', "deneme", str, 0x01, 0b0101,
+		-14, 0xff, 0xff);
+	printf("%c %s %p %d %i %u %x %X %%\n", 'c', "deneme", str, 0x01, 0b0101,
+		-14, 0xff, 0xff);
 }
